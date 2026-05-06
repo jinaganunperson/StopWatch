85347f87fa7c4ac8198e40e4ff4f5dd4b990dd1e
jinaganunperson <06younseo@gmail.com>
2026-05-06 22:14:23 +0900

    디자인 변경

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,11 +32,12 @@
             this.btnstop = new System.Windows.Forms.Button();
             this.lblTime = new System.Windows.Forms.Label();
             this.lblName = new System.Windows.Forms.Label();
+            this.btnstart = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnset
             // 
-            this.btnset.Font = new System.Drawing.Font("휴먼둥근헤드라인", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnset.Font = new System.Drawing.Font("휴먼둥근헤드라인", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.btnset.Location = new System.Drawing.Point(504, 208);
             this.btnset.Name = "btnset";
             this.btnset.Size = new System.Drawing.Size(164, 44);
@@ -46,8 +47,8 @@
             // 
             // btnstop
             // 
-            this.btnstop.Font = new System.Drawing.Font("휴먼둥근헤드라인", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.btnstop.Location = new System.Drawing.Point(156, 208);
+            this.btnstop.Font = new System.Drawing.Font("휴먼둥근헤드라인", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnstop.Location = new System.Drawing.Point(326, 208);
             this.btnstop.Name = "btnstop";
             this.btnstop.Size = new System.Drawing.Size(164, 44);
             this.btnstop.TabIndex = 6;
@@ -74,11 +75,22 @@
             this.lblName.TabIndex = 4;
             this.lblName.Text = "STOPWATCH";
             // 
+            // btnstart
+            // 
+            this.btnstart.Font = new System.Drawing.Font("휴먼둥근헤드라인", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnstart.Location = new System.Drawing.Point(154, 208);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(164, 44);
+            this.btnstart.TabIndex = 8;
+            this.btnstart.Text = "START";
+            this.btnstart.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 262);
+            this.Controls.Add(this.btnstart);
             this.Controls.Add(this.btnset);
             this.Controls.Add(this.btnstop);
             this.Controls.Add(this.lblTime);
@@ -96,6 +108,7 @@
         private System.Windows.Forms.Button btnstop;
         private System.Windows.Forms.Label lblTime;
         private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Button btnstart;
     }
 }
 
